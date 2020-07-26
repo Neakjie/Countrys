bde292129c9ca3e9221694c3f14a15d5e8e3a370
hanbai <zhao.hanb@husky.neu.edu>
2020-07-25 19:53:11 -0500

    search function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,12 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  searchedCountry:any;
+  searchedName:string;
+  searchForm = new FormGroup({});
   countrys: any;
   titles:any;
-  constructor(private httpClient:HttpClient){}
+  constructor(private httpClient:HttpClient, private fb: FormBuilder){}
 
   ngOnInit(): void {
     this.getCountrys().subscribe(data=>{
@@ -22,13 +26,40 @@ export class AppComponent implements OnInit {
 
       }
     })
+    this.searchForm = this.fb.group({
+      searchCountry:["",]
+    })
 
+  }
+
+  findCountry(controlName:string){
+    if(this.searchForm.get(controlName).value){
+      this.searchedName = this.searchForm.get(controlName).value
+    }
+    this.searchedCountry = this.countrys.filter(value => value.alpha2Code === this.searchedName ||
+                                                value.alpha2Code === this.searchedName.toUpperCase() ||
+                                                value.capital === this.searchedName ||
+                                                value.capital.toLowerCase() === this.searchedName ||
+                                                value.name.toLowerCase() === this.searchedName ||
+                                                value.name === this.searchedName)
+    console.log("country name:",this.searchedCountry);
+    if(this.searchedCountry.length === 0){
+      console.log("no Such Country Found");
+
+    }
+
+  }
+
+  cleanCountry(controlName:string){
+    this.searchedCountry = null;
+    this.searchForm.get('searchCountry').setValue("");
+    this.searchedName = "";
   }
 
   getCountrys():Observable<any>{
     let headers = new HttpHeaders({
       "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-      "x-rapidapi-key": [yourApiKey:"https://rapidapi.com/apilayernet/api/rest-countries-v1"]
+      "x-rapidapi-key": ["your api key"]
     })
     let options = { headers: headers,
                   body:{}
